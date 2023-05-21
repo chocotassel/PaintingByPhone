b6956bf1f659756611291c46d32ec4abfc27ebef
chocotassel <1694388268@qq.com>
2023-05-21 23:49:06 +0800

    init

--- App.js
+++ App.js
@@ -1,20 +1,93 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Gyroscope, Accelerometer, Magnetometer } from 'expo-sensors';
 
 export default function App() {
+  const [position, setPosition] = useState({ x: 0, y: 0, z: 0 });
+  const [isTracking, setIsTracking] = useState(false);
+
+  const [gyroData, setGyroData] = useState({ x: 0, y: 0, z: 0 });
+  const [accelData, setAccelData] = useState({ x: 0, y: 0, z: 0 });
+  const [magnetData, setMagnetData] = useState({ x: 0, y: 0, z: 0 });
+
+  useEffect(() => {
+    // 请求传感器权限
+    (async () => {
+      Gyroscope.requestPermissionsAsync();
+      Accelerometer.requestPermissionsAsync();
+      Magnetometer.requestPermissionsAsync();
+    })();
+  }, []);
+
+  const startTracking = async () => {
+    setIsTracking(true);
+
+    Gyroscope.setUpdateInterval(100);
+    Accelerometer.setUpdateInterval(100);
+    Magnetometer.setUpdateInterval(100);
+
+    Gyroscope.addListener((data) => setGyroData(data));
+    Accelerometer.addListener((data) => setAccelData(data));
+    Magnetometer.addListener((data) => setMagnetData(data));
+  };
+
+  const stopTracking = () => {
+    setIsTracking(false);
+    Gyroscope.removeAllListeners();
+    Accelerometer.removeAllListeners();
+    Magnetometer.removeAllListeners();
+    setPosition({ x: 0, y: 0, z: 0 });
+  };
+
+  useEffect(() => {
+    if (isTracking) {
+      // 这里是一个简化的计算，你可以使用更复杂数学模型来提高精度
+      setPosition({
+        x: (gyroData.x + accelData.x + magnetData.x) / 3,
+        y: (gyroData.y + accelData.y + magnetData.y) / 3,
+        z: (gyroData.z + accelData.z + magnetData.z) / 3,
+      });
+    }
+  }, [gyroData, accelData, magnetData, isTracking]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>X: {position.x.toFixed(2)}</Text>
+      <Text style={styles.text}>Y: {position.y.toFixed(2)}</Text>
+      <Text style={styles.text}>Z: {position.z.toFixed(2)}</Text>
+      {!isTracking ? (
+        <TouchableOpacity onPress={startTracking} style={styles.button}>
+          <Text style={styles.buttonText}>开始追踪</Text>
+        </TouchableOpacity>
+      ) : (
+        <TouchableOpacity onPress={stopTracking} style={styles.button}>
+          <Text style={styles.buttonText}>停止追踪</Text>
+        </TouchableOpacity>
+      )}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  flex: 1,
+  justifyContent: 'center',
+  alignItems: 'center',
+  backgroundColor: '#F5FCFF',
   },
-});
+  text: {
+  fontSize: 20,
+  textAlign: 'center',
+  margin: 10,
+  },
+  button: {
+  backgroundColor: '#0080FF',
+  padding: 15,
+  borderRadius: 5,
+  margin: 10,
+  },
+  buttonText: {
+  color: '#FFFFFF',
+  fontSize: 16,
+  },
+  });
